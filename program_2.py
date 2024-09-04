1029cf133c60f8ceea8cc72ad96b37f17e85dccf
sl14-828 <samlee8179@gmail.com>
2024-09-04 12:13:23 -0500

    First Update program_2.py
    Added the verse within the print statement, using "\ to implement the Quotation marks in the output and \n to create new lines in the same print statement.

--- program_2.py
+++ program_2.py
@@ -1,6 +1,8 @@
 def proverbs_22_29():
     # Write your Proverbs 22:29 code below
-    print('...')
+    print("\"Do you see a man skillful in his work?\nHe will stand before kings;"
+      "\nhe will not stand before obsecure men.\""
+      ' \n     (Proverbs 22:29)')
     
 
 proverbs_22_29()
