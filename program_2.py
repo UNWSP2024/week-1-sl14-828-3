acb80ee9c5a3e3a8121568c1c7da0600cc884812
sl14-828 <samlee8179@gmail.com>
2024-09-04 12:22:00 -0500

    Second Update program_2.py
    Usage of 4 print statements instead of using 1 with \n for new lines.

--- program_2.py
+++ program_2.py
@@ -1,8 +1,9 @@
 def proverbs_22_29():
     # Write your Proverbs 22:29 code below
-    print("\"Do you see a man skillful in his work?\nHe will stand before kings;"
-      "\nhe will not stand before obsecure men.\""
-      ' \n     (Proverbs 22:29)')
-    
+    print("\"Do you see a man skillful in his work?")
+    print("He will stand before kings;")
+    print("he will not stand before obsecure men.\"")
+    print("     (Proverbs 22:29)")
+
 
 proverbs_22_29()
